b4fe56d05594df5ce8a4dc02d8a05a7a80be9920
Nikita Nafranets <eddimensi@gmail.com>
2017-08-27 20:36:50 +0300

    wip

--- game.go
+++ game.go
@@ -1,12 +1,34 @@
 package main
 
 type Player struct {
+	Package PlayerPackage
 }
 
-type Item interface {
-	Do()
+func PlayerCreate() *Player {
+	player := &Player{}
+
+	return player
 }
 
 type PlayerPackage struct {
 	items map[string]Item
+}
+
+func (p *PlayerPackage) Init() {
+	p.items = make(map[string]Item)
+}
+
+func (p *PlayerPackage) Add(name string, item Item) {
+	p.items[name] = item
+}
+
+func (p *PlayerPackage) Remove(name string) {
+	delete(p.items, name)
+}
+
+type Room struct {
+}
+
+type Item interface {
+	Do()
 }
